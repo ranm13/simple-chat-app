03dccfffaad3c824dd9ce3b6474a0e47424c95cb
Ran Marciano <ranm13@gmail.com>
2019-10-24 16:01:38 +0300

    basic chat set up

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const socket = require('socket.io');
+const app = express();
+
+const PORT = 4000
+const server = app.listen(process.env.PORT || PORT, function(){
+    console.log('listening for requests on port 4000,');
+});
+
+app.use(express.static('public'));
+
+const io = socket(server);
+io.on('connection', (socket) => {
+    console.log('made socket connection', socket.id);
+
+    socket.on('chat', function(data){
+        io.sockets.emit('chat', data);
+    });
+
+    socket.on('typing', function(data){
+        socket.broadcast.emit('typing', data);
+    });
+});
---
+++ public/chat.js
@@ -0,0 +1,27 @@
+const socket = io.connect('https://cohort12-chat-rival.herokuapp.com/');
+const message = $('#message'),
+      handler = $('#handler'),
+      btn = $('#send'),
+      output = $('#output'),
+      feedback = $('#feedback');
+
+btn.on('click', function(){
+    socket.emit('chat', {
+        message: message.val(),
+        handler: handler.val()
+    });
+    message.val(" ") 
+});
+
+message.on('keypress', function(){
+    socket.emit('typing', handler.val());
+})
+
+socket.on('chat', function(data){
+    feedback.html(' ') 
+    output.append(`<p><strong>  ${data.handler}  : </strong>  ${data.message}  </p>`)  
+});
+
+socket.on('typing', function(data){
+    feedback.html(`<p><em>  ${data }  is typing a message...</em></p>`)  
+});
